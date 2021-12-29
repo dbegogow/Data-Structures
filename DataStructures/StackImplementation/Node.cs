b40de4b9e9ed416eca88f06429f8242a4d55d18c
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-29 22:04:11 +0200

    Create stack implementation project and node class

---
+++ DataStructures/StackImplementation/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace StackImplementation
+{
+    public class Node<T>
+    {
+        public Node(T value)
+            => this.Value = value;
+
+        public T Value { get; }
+
+        public Node<T> Next { get; set; }
+    }
+}
