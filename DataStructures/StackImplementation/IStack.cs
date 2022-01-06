01ebf15b3c7e63632afa34f60d6fb7189afa18b0
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-01-06 17:31:35 +0200

    Implement stack interface

---
+++ DataStructures/StackImplementation/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace StackImplementation
+{
+    public interface IStack<T> : IEnumerable<T>
+    {
+        int Count { get; }
+
+        void Push(T item);
+
+        T Pop();
+
+        T Peek();
+
+        bool Contains(T item);
+    }
+}
---
+++ DataStructures/StackImplementation/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace StackImplementation
+{
+    public class Stack<T> : IStack<T>
+    {
+        public int Count { get; }
+
+        public void Push(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
