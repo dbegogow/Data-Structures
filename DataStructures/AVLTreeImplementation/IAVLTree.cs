6a2d3d01258a9d0d9f7723835fb9d7794104a58d
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-07-23 22:23:38 +0300

    Create IAVLTree interface

---
+++ DataStructures/AVLTreeImplementation/IAVLTree.cs
@@ -0,0 +1,13 @@
+﻿namespace AVLTreeImplementation
+{
+    public interface IAVLTree<T> where T : IComparable<T>
+    {
+        bool Contains(T item);
+
+        void Insert(T item);
+
+        void EachInOrder(Action<T> action);
+
+        Node<T> Insert(Node<T> node, T value);
+    }
+}
--- DataStructures/AVLTreeImplementation/Node.cs
+++ DataStructures/AVLTreeImplementation/Node.cs
@@ -2,6 +2,12 @@
 {
     public class Node<T> where T : IComparable<T>
     {
+        public Node(T value)
+        {
+            this.Value = value;
+            this.Height = 1;
+        }
+
         public T Value { get; }
 
         public Node<T> Left { get; set; }
