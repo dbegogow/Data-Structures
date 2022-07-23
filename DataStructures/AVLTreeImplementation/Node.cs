e2bd9c88d8675a7984b9de6748c52e2a6ae34022
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-07-23 22:19:36 +0300

    Create AVL Tree implementation project and node model

---
+++ DataStructures/AVLTreeImplementation/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace AVLTreeImplementation
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public T Value { get; }
+
+        public Node<T> Left { get; set; }
+
+        public Node<T> Right { get; set; }
+
+        public int Height { get; set; }
+    }
+}
