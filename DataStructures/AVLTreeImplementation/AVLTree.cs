2588a6e31d2781814ebba89f77d920e888c95746
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-07-24 18:03:15 +0300

    Implement Insert method in AVL Tree implementation

--- DataStructures/AVLTreeImplementation/AVLTree.cs
+++ DataStructures/AVLTreeImplementation/AVLTree.cs
@@ -34,7 +34,80 @@
                 node.Right = this.Insert(node.Right, item);
             }
 
+            this.UpdateHeight(node);
+
+            node = this.Balance(node);
+
             return node;
+        }
+
+        private Node<T> Balance(Node<T> node)
+        {
+            var balance = this.Height(node.Left) - this.Height(node.Right);
+            if (balance > 1)
+            {
+                var childBalance = this.Height(node.Left.Left) - this.Height(node.Left.Right);
+                if (childBalance < 0)
+                {
+                    node.Left = this.RotateLeft(node.Left);
+                }
+
+                node = this.RotateRight(node);
+            }
+            else if (balance < -1)
+            {
+                var childBalance = this.Height(node.Right.Left) - this.Height(node.Right.Right);
+                if (childBalance > 0)
+                {
+                    node.Right = this.RotateRight(node.Right);
+                }
+
+                node = this.RotateLeft(node);
+            }
+
+            return node;
+        }
+
+        private Node<T> RotateLeft(Node<T> node)
+        {
+            var right = node.Right;
+            node.Right = right.Left;
+            right.Left = node;
+
+            this.UpdateHeight(node);
+            this.UpdateHeight(right);
+
+            return right;
+        }
+
+        private Node<T> RotateRight(Node<T> node)
+        {
+            var left = node.Left;
+            node.Left = left.Right;
+            left.Right = node;
+
+            this.UpdateHeight(node);
+            this.UpdateHeight(left);
+
+            return left;
+        }
+
+        private void UpdateHeight(Node<T> node)
+        {
+            if (node != null)
+            {
+                node.Height = Math.Max(this.Height(node.Left), this.Height(node.Right)) + 1;
+            }
+        }
+
+        private int Height(Node<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return node.Height;
         }
     }
 }
--- DataStructures/TryDataStructures/Startup.cs
+++ DataStructures/TryDataStructures/Startup.cs
@@ -9,15 +9,15 @@ namespace TryDataStructures
             var avlTree = new AVLTree<int>();
 
             avlTree.Insert(10);
-            avlTree.Insert(9);
-            avlTree.Insert(8);
-            avlTree.Insert(40);
+            avlTree.Insert(5);
+            avlTree.Insert(20);
+            avlTree.Insert(34);
+            avlTree.Insert(1);
+            avlTree.Insert(22);
+            avlTree.Insert(28);
+            avlTree.Insert(45);
+            avlTree.Insert(3);
             avlTree.Insert(50);
-            avlTree.Insert(60);
-            avlTree.Insert(70);
-            avlTree.Insert(80);
-
-            ;
         }
     }
 }
